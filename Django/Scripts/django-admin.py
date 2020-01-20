ff19fc983ffe5241d0e0ef41b406c2729d621240
Caesark1 <kebec2015@gmail.com>
2020-01-21 04:47:48 +0600

    first commit

---
+++ Django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\programming\it academy\django\django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
